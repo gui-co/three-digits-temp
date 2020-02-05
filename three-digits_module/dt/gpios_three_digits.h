3b404438971d7e5922d74143f98b75dfef671fe6
Guillaume Communie <guillaume.communie@gmail.com>
2020-02-05 16:50:41 +0100

    DT: header to define used GPIOs
    A header file allows the assigment of needed GPIOs.

---
+++ three-digits_module/dt/gpios_three_digits.h
@@ -0,0 +1,18 @@
+#ifndef GPIOS_THREE_DIGITS_H
+#define GPIOS_THREE_DIGITS_H
+
+#define GPIO_DIGIT_1_POWER 2
+#define GPIO_DIGIT_2_POWER 3
+#define GPIO_DIGIT_3_POWER 4
+
+#define GPIO_SEG_A         7
+#define GPIO_SEG_B         8
+#define GPIO_SEG_C         9
+#define GPIO_SEG_D         10
+#define GPIO_SEG_E         11
+#define GPIO_SEG_F         14
+#define GPIO_SEG_G         15
+#define GPIO_DOT           17
+
+#endif /* GPIOS_THREE_DIGITS_H */
+
